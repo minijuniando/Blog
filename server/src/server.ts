51398a55c91fd324c9629454c95a0c18f1892f2e
albqvictor1508 <albq.victor@gmail.com>
2025-07-15 18:51:54 -0300

    chore: app listen and error handling

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,16 @@ import express from "express";
 
 export const app = express();
 
+app.use(express.json());
+
 app.get("/health", (request, response) => {
   return response.status(200).send("OK");
+})
+
+app.listen(3333, (err: Error | undefined): void => {
+  if (err) {
+    console.log(err);
+    throw err;
+  }
+  console.log("HTTP Server Running!");
 })
