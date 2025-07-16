951cbb8d7fc551ba3b8dff0e8e5ea3359ec51c29
albqvictor1508 <albq.victor@gmail.com>
2025-07-16 14:41:36 -0300

    chore: creating modelation v1

--- server/src/db/schema/index.ts
+++ server/src/db/schema/index.ts
@@ -1,13 +1,30 @@
 import { db } from "../client";
 
 const articleSchema = new db.Schema({
+  title: String,
+  userId: String,
+
+});
+
+export const Article = db.model("articles", articleSchema);
+
+const techTagSchema = new db.Schema({
   name: String
 });
-export const Article = await db.model("articles", articleSchema);
 
-const techTagSchema = new db.Schema({ name: String });
-export const TechTag = await db.model("tech_tags", techTagSchema);
+export const TechTag = db.model("tech_tags", techTagSchema);
 
-const userSchema = new db.Schema({ name: String, email: String, password: String });
-export const User = await db.model("users", userSchema);
+const userSchema = new db.Schema({
+  name: String,
+  email: String,
+  password: String
+});
+
+export const User = db.model("users", userSchema);
+
+const likeSchema = new db.Schema({
+  userId: String,
+  postId: String,
+});
+
 
