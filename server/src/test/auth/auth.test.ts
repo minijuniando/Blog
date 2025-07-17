be3991ffdbbddd0f5bea7b69ef0dd5913f79df31
albqvictor1508 <albq.victor@gmail.com>
2025-07-17 18:11:22 -0300

    feat: jwt completed and tests running

--- server/src/test/auth/auth.test.ts
+++ server/src/test/auth/auth.test.ts
@@ -66,7 +66,7 @@ describe("Middleware: validateUser", () => {
     await validateUser(req, res, next);
 
     expect(next).toHaveBeenCalled();
-    expect((req as any).user).toEqual(userPayload);
+    expect((req as any).user).toEqual(userPayload); //VOU TIRAR ISSO DEPOIS SE NÃO FOR PRECISO NA ROTA
     expect(res.status).not.toHaveBeenCalled();
     expect(res.send).not.toHaveBeenCalled();
   });
