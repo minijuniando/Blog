120529c8031e9e2ac5439852c5c0d33676da35c7
albqvictor1508 <albq.victor@gmail.com>
2025-07-17 09:21:57 -0300

    chore: temporary changes on middleware

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,13 +1,13 @@
-import jwt, { JwtPayload, VerifyErrors } from "jsonwebtoken";
+import jwt, { type JwtPayload, type VerifyErrors } from "jsonwebtoken";
 import { env } from "../common/env";
-import { type Request, type Response } from "express";
+import type { Request, Response } from "express";
 
 export async function validateUser(
   request: Request,
   response: Response,
   next: () => unknown,
 ) {
-  const authHeader = request.headers["authorization"];
+  const { authorization: authHeader } = request.headers;
   const token = authHeader && authHeader.split(" ")[1];
 
   if (!token) return response.status(401).send();
@@ -18,7 +18,7 @@ export async function validateUser(
     (err: VerifyErrors | null, user: string | JwtPayload | undefined) => {
       console.log(err);
       if (err || !user) return response.status(403).send(err);
-      request.user = user;
+      (request as any).user = user;
       next();
     },
   );
