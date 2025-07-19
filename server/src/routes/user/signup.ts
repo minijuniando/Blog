3731d204c431dcd4cc13d7056ccc529f56e79ab8
albqvictor1508 <albq.victor@gmail.com>
2025-07-19 00:24:18 -0300

    chore: little changes on zod schema

--- server/src/routes/user/signup.ts
+++ server/src/routes/user/signup.ts
@@ -7,60 +7,16 @@ import { db } from "../../db/client";
 import { verifyEmailHtml } from "../../utils/html/verify-email";
 import { handleSendEmail } from "../../utils/send-email";
 import { roles } from "../../types/user";
+import { validateSignup } from "../../schema/user";
 
 export const signup = async (
   request: Request,
   response: Response,
 ): Promise<unknown> => {
   try {
-    const { username, email, password, confirmPassword, role } = request.body;
-    if (!username || !email || !password || !confirmPassword) {
-      return response.status(400).json({
-        success: false,
-        message: "Todos os campos são obrigatórios",
-      });
-    }
-
-    if (!isValidEmail(email)) {
-      return response.status(400).json({
-        success: false,
-        message: "Email inválido",
-      });
-    }
-
-    if (password !== confirmPassword) {
-      return response.status(400).json({
-        success: false,
-        message: "Senhas não coincidem",
-      });
-    }
-
-    if (password.length < 6)
-      return response.status(400).json({
-        success: false,
-        message: "Senha deve ter pelo menos 6 caracteres",
-      });
-
-    if (username.length < 6) {
-      return response.status(400).json({
-        success: false,
-        message: "Nome deve ter pelo menos 2 caracteres",
-      });
-    }
-
-    if (!Object.values(role).includes(role)) {
-      return response.status(400).json({
-        success: false,
-        message: "Invalid role",
-      });
-    }
-
-    if (username.includes(" ")) {
-      return response.status(400).json({
-        success: false,
-        message: "Nome não pode conter espaços",
-      });
-    }
+    const { username, email, password, role } = await validateSignup(
+      request.body,
+    );
 
     const existingUser = await db.user.findUnique({
       where: { email: email.toLowerCase() },
@@ -107,6 +63,12 @@ export const signup = async (
     }
     return response.status(201).send({ user, jwt: token });
   } catch (error) {
+    if (error instanceof z.ZodError) {
+      return response.status(400).json({
+        success: false,
+        message: error.errors[0].message,
+      });
+    }
     console.error(chalk.red(error));
 
     response.status(500).json({
--- server/src/schema/user.ts
+++ server/src/schema/user.ts
@@ -1,1 +1,27 @@
-export const signupSchema = 
+import type { Request } from "express";
+import z from "zod";
+import { roles } from "../types/user";
+
+export async function validateSignup(
+  body: unknown,
+): Promise<z.infer<typeof signupSchema>> {
+  const signupSchema = z.object({
+    username: z
+      .string()
+      .min(2, "Nome deve ter pelo menos 2 caracteres")
+      .max(30, "Username too long")
+      .refine((name) => !name.includes(" "), {
+        message: "Nome não pode conter espaços",
+      }),
+    email: z.email("Email inválido"),
+    password: z.string().min(6, "Senha deve ter pelo menos 6 caracteres"),
+    confirmPassword: z
+      .string("Confirm password is required")
+      .refine((data) => data.password === data.confirmPassword, {
+        message: "Senhas não coincidem",
+        path: ["confirmPassword"],
+      }),
+    role: z.enum(Object.values(roles), "Invalid role"),
+  });
+  return signupSchema.parseAsync(body);
+}
