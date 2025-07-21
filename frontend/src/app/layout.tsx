eb42e732d534f14be020c329cb8afeb388738302
Marcus Vinicius <marcusv892@gmail.com>
2025-07-21 18:01:51 -0300

    feat: update config project, added plugin import sort

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,15 +1,12 @@
-import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
-})
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
+const inter = Inter({
 	subsets: ['latin'],
+	weight: ['400', '500', '600'],
+	variable: '--font-inter',
 })
 
 export const metadata: Metadata = {
@@ -24,9 +21,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
+			<body className={`${inter.className} bg-background antialiased`}>
 				{children}
 			</body>
 		</html>
