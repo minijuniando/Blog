b591192d6a3d87a0208e709aa9d86c4844aefd84
albqvictor1508 <albq.victor@gmail.com>
2025-07-23 10:53:48 -0300

    refactor: better response from login route

--- server/src/function/user/login.ts
+++ server/src/function/user/login.ts
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import type { ErrorSchema } from "../../types";
+import { db } from "../../db/client";
 
 export async function login({
 	email,
@@ -9,10 +10,11 @@ export async function login({
 	email: string;
 	password: string;
 }): Promise<{ token: string } | ErrorSchema> {
-	const user = await db.user.findFirst({
+	const user = await db.user.findUnique({
 		where: { email },
 	});
-	if (!user) return response.status(400).send("Este usuário não existe");
+	if (!user)
+		return { error: true, status: 400, message: "Esse usuário já existe" };
 
 	const hashedPassword = user.password;
 	const comparedPassword = await bcrypt.compare(password, hashedPassword);
