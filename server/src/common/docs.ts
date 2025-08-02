19ad67f086959e099bf27c3e0e32deca913d93b7
albqvictor1508 <albq.victor@gmail.com>
2025-08-02 12:13:23 -0300

    chore: creating swagger integration

--- server/src/common/docs.ts
+++ server/src/common/docs.ts
@@ -1,2 +1,15 @@
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUI from "swagger-ui-express";
+
+const swaggerSpec = swaggerJsdoc({
+	definition: {
+		info: {
+			title: "Mini-juniando",
+			description: "Mini-juniando API endpoints",
+			version: "1.0.0",
+		},
+	},
+	apis: ["../routes/*.ts"],
+});
+
+export function swaggerDocs(): void {}
